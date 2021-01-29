8f8ed7bbc9ba5c3590f8e1f0134af800ee999a94
JustNao <nao.3nicolas@hotmail.fr>
2021-01-29 16:18:14 +0100

    Repo creation

---
+++ treasureHunt/treasureHuntBot.py
@@ -0,0 +1,303 @@
+from asyncio.windows_events import NULL
+
+from PIL.ImageOps import grayscale
+from sniffer import protocol
+import requests
+import time
+import os, sys
+from colorama import init, Fore, Style
+from sources.id import mapIdToCoords, textId, poiToName, npcToName, monsterToName, archiNameList
+import ui.gui as g
+import pyautogui as ag
+from random import random
+import math
+
+init()  # Don't touch, used for colors in terminal
+
+class TreasureHuntHelper():
+
+    def __init__(self, bot=False) -> None:
+        # dict.loadIds()
+        global DEBUG
+        DEBUG = False
+        print("Treasure helper initialized")
+        self.botting = bot
+        # MapChange, TreasureStep, TreasurePOI, TreasureHinT, MapComplementaryInfo, FightStart
+        self.interestingPackets = [2889, 5745, 7529, 9917, 2291, 4119, 9401]
+        self.playerPos = self.Position()
+        self.hintPos = self.Hint()
+        self.timeStart = NULL
+        self.direction = "stay"
+        self.clientHintName = NULL
+        self.checkPositions = []
+        self.phorreur = {
+            "lookingFor": False,
+            "npcId": 2673
+        }
+        if self.botting:
+            self.initBotting()
+        if getattr(sys, 'frozen', False):
+            # If the application is run as a bundle, the PyInstaller bootloader
+            # extends the sys module by a flag frozen=True and sets the app 
+            # path into variable _MEIPASS'.
+            self.application_path = sys._MEIPASS
+        else:
+            self.application_path = os.path.dirname(os.path.abspath(__file__))
+
+    class Hint:
+        def __init__(self, posX=0, posY=0, d=666):
+            self.x = posX
+            self.y = posY
+            self.distance = d
+
+        def __str__(self):
+            return '[' + str(self.x) + ',' + str(self.y) + ']'
+
+    class Position:
+        def __init__(self, posX=0, posY=0):
+            self.x = posX
+            self.y = posY
+
+        def __str__(self):
+            return '[' + str(self.x) + ',' + str(self.y) + ']'
+
+    def initBotting(self):
+        # Initialisation du bot, fixe les positions de changement de carte
+        ag.FAILSAFE = True
+        self.sizeX, self.sizeY = ag.size()
+        bottomScreen = ag.locateOnScreen(os.path.dirname(os.path.realpath(
+            '__file__')) + '\\sources\\img\\pixel\\bottomScreen.png', grayscale=True, confidence=.75)
+        if bottomScreen is None:
+            print(
+                Fore.RED + "Error : please keep your Dofus window open for the initilization" + Style.RESET_ALL)
+            g.ui.changeText("RESTART")
+            return
+
+        self.movePos = {
+            "right": (self.sizeX - 50, self.sizeY/2),
+            "left": (50, self.sizeY/2),
+            "top": (self.sizeX/2, 10),
+            "bottom": (bottomScreen.left, bottomScreen.top - 10)
+        }
+
+        self.WAIT_TIME = 2
+
+    def move(self):
+        # Déplacement du bot
+        if (not self.botting) or (self.hintPos.distance > 10):
+            return
+
+        currentMousePos = ag.position()
+        # time.sleep(random()*2) # Temps de pause entre chaque action, risqué car fait attendre le thread entier
+        try:
+            ag.PAUSE = 0.2
+            x, y = self.movePos[self.direction]
+            ag.moveTo(x, y)
+            time.sleep(0.2)
+            while ag.locateOnScreen(os.path.dirname(os.path.realpath(
+                    '__file__')) + '\\sources\\img\\pixel\\groupOnMouse.png', grayscale=True, confidence=0.75) is not None:
+                x += random()*20 - 10
+                ag.moveTo(x, y)
+            ag.PAUSE = 0
+            print("Bot moved", self.direction)
+            ag.click(x, y)
+        except KeyError:
+            print("Bot can't move, need to stay put")
+
+        ag.moveTo(currentMousePos)
+
+    def packetRead(self, msg):
+        if msg.id in self.interestingPackets:
+            try:
+                packet = protocol.read(
+                    protocol.msg_from_id[msg.id]["name"], msg.data)
+            except KeyError:
+                print("KeyError ", msg.id)
+                return
+            if msg.id == 2889:  # Changement de Map
+                self.changeMap(packet)
+            elif msg.id == 5745:  # Nouvelle étape de chasse aux trésors
+                self.huntNewStep(packet)
+            elif (msg.id == 2291):  # Chargement de carte, recherche d'un phorreur
+                self.mapContentAnalyse(packet)
+            elif (msg.id == (4119 or 9401)):
+                # En entrant en combat, on met en pause le sniffer pour éviter les problèmes
+                g.ui.load()
+        if DEBUG:
+            print(protocol.msg_from_id[msg.id]["name"])
+
+    def changeMap(self, packet):
+        self.playerPos = self.Position(
+            mapIdToCoords[packet['mapId']][0], mapIdToCoords[packet['mapId']][1])
+        print("New position : [", self.playerPos.x, ',', self.playerPos.y, ']')
+        self.stepUpdate()
+
+    def stepUpdate(self):
+        # Actualise le modèle à chaque changement de carte #
+
+        if (self.hintPos.distance <= 10) and (abs(self.playerPos.x - self.hintPos.x) <= 10) and (abs(self.playerPos.y - self.hintPos.y) <= 10) and (not self.phorreur['lookingFor']):
+            if self.playerPos.x != self.hintPos.x:
+                self.hintPos.distance = abs(self.playerPos.x - self.hintPos.x)
+
+                g.ui.changeImg(str(int(self.hintPos.distance)))
+                print("Horizontal distance from hint : ", Fore.GREEN +
+                      str(self.hintPos.distance) + Style.RESET_ALL)
+
+                if self.playerPos.x < self.hintPos.x:
+                    direction = 'right'
+                else:
+                    direction = 'left'
+
+                g.ui.changeDirection(direction)
+                self.direction = direction
+
+            elif self.playerPos.y != self.hintPos.y:
+                self.hintPos.distance = abs(self.playerPos.y - self.hintPos.y)
+
+                print("Vertical distance from hint : ", Fore.GREEN +
+                      str(self.hintPos.distance) + Style.RESET_ALL)
+                g.ui.changeImg(str(int(self.hintPos.distance)))
+
+                if self.playerPos.y < self.hintPos.y:
+                    direction = 'bottom'
+                else:
+                    direction = 'top'
+
+                g.ui.changeDirection(direction)
+                self.direction = direction
+
+            else:
+                print(Fore.RED + "Hint found !" + Style.RESET_ALL)
+                g.ui.changeImg("found")
+                g.ui.changeDirection()
+                self.direction = "stay"
+                time.sleep(0.5)
+                if self.botting:
+                    g.ui.clickNextStep()
+
+    def reset(self):
+        self.hintPos.distance = 666
+        self.direction = "stay"
+        self.phorreur['lookingFor'] = False
+
+    def mapContentAnalyse(self, packet):
+        for actor in packet["actors"]:
+            if (actor['__type__'] == "GameRolePlayTreasureHintInformations"):
+                if (actor['npcId'] == self.phorreur['npcId']):
+                    print(Fore.RED + "Phorreur found !" + Style.RESET_ALL)
+                    self.phorreur['lookingFor'] = False
+                    self.hintPos = self.Hint(
+                        self.playerPos.x, self.playerPos.y, 0)
+
+                    g.ui.changeImg('found')
+                    self.direction = "stay"
+                    g.ui.changeDirection()
+                    if self.botting:
+                        g.ui.clickNextStep()
+
+                    break
+            elif (actor['__type__'] == 'GameRolePlayGroupMonsterInformations'):
+                mainMob = actor['staticInfos']['mainCreatureLightInfos']['genericId']
+                if monsterToName(mainMob) in archiNameList:
+                    print(Fore.BLUE + "Archimonstre found !" + Style.RESET_ALL)
+                    g.ui.changeImg('archimonstre')
+                    g.ui.load()
+                    self.direction = "stay"
+                    break
+
+
+        self.move()
+
+    def huntNewStep(self, packet):
+        self.reset()
+
+        if len(packet['flags']) == 0:
+            self.checkPositions.clear()
+            self.checkPositions.append(
+                (mapIdToCoords[packet['startMapId']][0], mapIdToCoords[packet['startMapId']][1]))
+            if (packet['checkPointCurrent'] == 0):
+                self.timeStart = time.time()
+                self.playerPos = self.Position(-25, -36)  # Malle aux trésors
+                self.direction = "stay"
+                g.ui.changeDirection()
+        elif len(packet['flags']) == packet['totalStepCount']:
+            g.ui.changeImg("checkpoint")
+            if self.botting():
+                g.ui.clickNextStep()
+            g.ui.changeDirection()
+            return
+
+        if packet['checkPointCurrent'] >= packet['checkPointTotal'] - 1:
+            end = time.time()
+            print(Fore.MAGENTA + "Treasure found !" + Style.RESET_ALL)
+            print("This hunt took ", Fore.CYAN + str(int(end -
+                                                         self.timeStart)) + Style.RESET_ALL, " seconds to finish")
+            g.ui.changeImg('combat')
+            g.ui.load()
+            self.direction = "stay"
+            return
+
+        intDirection = packet['knownStepsList'][-1]['direction']
+        if intDirection == 0:
+            strDirection = 'right'
+        elif intDirection == 2:
+            strDirection = 'bottom'
+        elif intDirection == 4:
+            strDirection = 'left'
+        else:
+            strDirection = 'top'
+        self.direction = strDirection
+
+        if packet['knownStepsList'][-1]['__type__'] == 'TreasureHuntStepFollowDirectionToPOI':
+            # Using Dofus-Map API to get hints based on position and direction
+            if (len(packet['flags']) == 0):
+                lastCheckPoint = packet['startMapId']
+            else:
+                lastCheckPoint = packet['flags'][-1]['mapId']
+
+            url = 'https://dofus-map.com/huntTool/getData.php?x=' + str(mapIdToCoords[lastCheckPoint][0]) + '&y=' + str(
+                mapIdToCoords[lastCheckPoint][1]) + '&direction=' + strDirection + '&world=0&language=fr'
+            get = requests.get(url)
+            js = get.json()
+
+            clientHintName = poiToName(
+                packet['knownStepsList'][-1]['poiLabelId'])
+
+            for hint in js['hints']:
+                dofusMapName = textId[str(hint['n'])]
+                if clientHintName == dofusMapName:
+                    if (hint['d'] < self.hintPos.distance) and ((hint['x'], hint['y']) not in self.checkPositions):
+                        self.hintPos = self.Hint(
+                            hint['x'], hint['y'], hint['d'])
+
+            if self.hintPos.distance == 666:
+                print(Fore.RED + "ERROR : no hint found !" + Style.RESET_ALL)
+                print(packet)
+                g.ui.changeText("No hint found")
+                g.ui.changeImg('found')
+                self.direction = "stay"
+            else:
+                self.checkPositions.append((self.hintPos.x, self.hintPos.y))
+                print(Fore.YELLOW + clientHintName + Style.RESET_ALL + " found " + Fore.GREEN + str(self.hintPos.distance) + Style.RESET_ALL +
+                      " maps " + Fore.GREEN + strDirection + Style.RESET_ALL + " at " + Fore.YELLOW + self.hintPos.__str__() + Style.RESET_ALL)
+                g.ui.changeText(self.hintPos.__str__())
+                g.ui.changeDirection(strDirection)
+                g.ui.changeImg(str(int(math.sqrt(math.pow(
+                    self.playerPos.y - self.hintPos.y, 2) + math.pow(self.playerPos.x - self.hintPos.x, 2)))))
+                self.stepUpdate()
+                self.move()
+
+        else:
+            clientHintName = npcToName(packet['knownStepsList'][-1]['npcId'])
+            print("Phorreur to find ", Fore.GREEN +
+                  strDirection + Style.RESET_ALL)
+            self.phorreur['lookingFor'] = True
+            self.phorreur['npcId'] = packet['knownStepsList'][-1]['npcId']
+            g.ui.changeDirection(strDirection)
+            g.ui.changeImg("phorreur")
+
+            if (len(packet['flags']) == 0) and (packet['checkPointCurrent'] == 0):
+                # Si un phorreur est détécté en tout premier, il ne faut pas commencer à le chercher
+                self.direction = "stay"
+
+            self.move()
---
+++ ui/gui.py
@@ -0,0 +1,200 @@
+from tkinter.constants import CENTER, RIGHT
+import PySimpleGUI as sg
+import os, sys
+import threading
+import pyautogui as ag
+from treasureHunt.treasureHuntBot import TreasureHuntHelper
+
+
+
+class GraphicalInterface():
+    def __init__(self, startSniff):
+        ag.PAUSE = 0
+        self.found = None
+        self.packetRead = None
+        self.startSniff = startSniff
+        self.stopSniff = None
+        self.botting = True
+    
+    def botInitialisation(self):
+        self.packetRead = TreasureHuntHelper(self.botting).packetRead
+        self.load()
+
+    def load(self):
+        if self.stopSniff is None:
+            self.stopSniff = self.startSniff(self.packetRead)
+            print("Sniffer started")
+            try:
+                botWindow['ON/OFF'].update(image_filename=imgList['on'])
+            except NameError:
+                pass
+        else:
+            self.stop() 
+
+    def stop(self):
+        if self.stopSniff is not None:
+            self.stopSniff()
+            self.stopSniff = None
+            print("Sniffer stopped")
+            try:
+                botWindow['ON/OFF'].update(image_filename=imgList['off'])
+            except NameError:
+                pass 
+            
+    def startUi(self):
+        t = threading.Thread(target=self.buildUi, name="GUI")
+        t.start()
+
+    def buildUi(self):
+        global imgList, application_path
+
+        def fileName(file) -> str:
+            pointInd = file.find('.')
+            return file[:pointInd]
+
+        if getattr(sys, 'frozen', False):
+            # If the application is run as a bundle, the PyInstaller bootloader
+            # extends the sys module by a flag frozen=True and sets the app 
+            # path into variable _MEIPASS'.
+            application_path = sys._MEIPASS
+        else:
+            application_path = os.path.dirname(os.path.abspath(__file__))
+
+        sg.LOOK_AND_FEEL_TABLE['TreasureHunt'] = {'BACKGROUND': '#d4c194',
+                                                  'TEXT': 'black',
+                                                  'INPUT': '#DDE0DE',
+                                                  'SCROLL': '#E3E3E3',
+                                                  'TEXT_INPUT': 'black',
+                                                  'BUTTON': ('white', '#6D9F85'),
+                                                  'PROGRESS': 'white',
+                                                  'BORDER': 1,
+                                                  'SLIDER_DEPTH': 0,
+                                                  'PROGRESS_DEPTH': 0}
+
+        imgFolder = application_path + '\\..\\sources\\img\\GUI'
+
+        imgList = {fileName(str(f)): imgFolder + '\\' +
+                   f for f in os.listdir(imgFolder) if '.png' in f}
+
+        # MENU #
+        sg.theme('DarkBlue')
+
+        menuLayout =[
+                [
+                    sg.Radio("Treasure Hunt Bot", default = True, group_id = "CHOICE", key = 'huntBot'),
+                    sg.Radio("Treasure Hunt Helper", group_id = "CHOICE", key = 'huntHelper')
+                ],
+                [sg.Button("Launch", key = "LAUNCH")]
+            ]
+
+        menuWindow = sg.Window(
+            title = "DHM",
+            layout = menuLayout,
+            margins = (20, 5),
+            border_depth=3,
+            keep_on_top=False,
+            finalize=True,
+            element_justification=CENTER,
+            icon = application_path + '\\..\\sources\\img\\icon\\phoenix.ico'
+            )
+
+        while True:
+            event, values = menuWindow.read()
+            if event == "LAUNCH":
+                for key in values:
+                    if values[key]:
+                        self.userChoice = key
+                        self.botting = values['huntBot']
+                        break
+                break
+            elif (event == sg.WIN_CLOSED):
+                break
+        menuWindow.close()
+        try:
+            if (self.userChoice == 'huntHelper') or (self.userChoice == 'huntBot'):
+                self.startTreasureHuntUi()
+        except AttributeError:
+            return
+
+    def startTreasureHuntUi(self):
+        global botWindow
+        self.botInitialisation()
+        sg.theme('TreasureHunt')
+        botLayout = [
+            [
+                sg.Button(image_filename=imgList['on'], button_color=(sg.theme_background_color(
+                ), sg.theme_background_color()), border_width=0, key="ON/OFF", pad=(10, 0)),
+                sg.Button(image_filename=imgList['exit'], button_color=(sg.theme_background_color(
+                ), sg.theme_background_color()), border_width=0, key='EXIT', image_size=(30, 30))
+            ],
+            [sg.Text("Position info", key="POS",
+                     font=('Helvetica', 15, 'bold'))],
+            [sg.HorizontalSeparator()],
+            [sg.Image(filename=imgList['noDirection'], key="DIRECTION")],
+            [sg.Button(image_filename=imgList['placeHolder'], button_color=(
+                sg.theme_background_color(), sg.theme_background_color()), border_width=0, key="IMAGE")]
+        ]
+
+        botWindow = sg.Window(
+            title="Treasure Hunt Helper",
+            no_titlebar=True,
+            grab_anywhere=True,
+            layout=botLayout,
+            margins=(0, 0),
+            border_depth=3,
+            keep_on_top=True,
+            finalize=True,
+            alpha_channel=.85,
+            element_justification=CENTER)
+
+        while True:
+            event, values = botWindow.read(timeout=15000)
+            if event == "IMAGE":
+                if self.found is not None:
+                    self.clickNextStep()
+            elif event == "ON/OFF":
+                self.load()
+            elif (event == sg.WIN_CLOSED) or (event == "EXIT"):
+                self.stopSniff()
+                print("Sniffer ended")
+                break
+            botWindow.refresh()
+
+        botWindow.close()
+
+    def changeImg(self, imgName):
+        if (imgName == "found") or (imgName == "checkpoint") or (imgName == "combat"):
+            self.found = imgName
+        try:
+            botWindow["IMAGE"].update(image_filename=imgList[imgName])
+        except KeyError:
+            botWindow["IMAGE"].update(image_filename=imgList['tooFar'])
+
+    def changeText(self, pos):
+        botWindow["POS"].update(pos)
+
+    def changeDirection(self, dir="noDirection"):
+        botWindow["DIRECTION"].update(filename=imgList[dir])
+
+    def clickNextStep(self):
+        currentX, currentY = ag.position()
+        if self.found == "found":
+            pos = ag.locateCenterOnScreen(application_path + '\\..\\sources\\img\\pixel\\' + 'flag.png', grayscale=True, confidence=.8)
+            ag.leftClick(pos[0], pos[1])
+        elif self.found == "checkpoint":
+            pos = ag.locateCenterOnScreen(application_path + '\\..\\sources\\img\\pixel\\' + 'checkpoint.png', grayscale=True, confidence=.8)
+            ag.leftClick(pos[0], pos[1])
+        elif self.found == "combat":
+            pos = ag.locateCenterOnScreen(application_path + '\\..\\sources\\img\\pixel\\' + 'combat.png', grayscale=True, confidence=.8)
+            ag.leftClick(pos[0], pos[1])
+        else:
+            print("No next step detected :(")
+
+        ag.moveTo(currentX, currentY)
+        self.found = None
+
+
+def init(startSniff):
+    global ui
+    ui = GraphicalInterface(startSniff)
+    ui.startUi()
