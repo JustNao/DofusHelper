990147c4a80fdb7efdfd4a4f18417f4bfe0d3b1c
JustNao <nao.3nicolas@hotmail.fr>
2022-08-21 17:04:08 +0200

    Packets for 2.64.9

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DofusInvoker="C:\Users\USERNAME\AppData\Local\Ankama\zaap\Dofus\DofusInvoker.swf"
+export DofusInvoker="C:\Users\USERNAME\AppData\Local\Ankama\Dofus\DofusInvoker.swf"
 export selectclass='com.ankamagames.dofus.BuildInfos,com.ankamagames.dofus.network.++,com.ankamagames.jerakine.network.++'
 export config='parallelSpeedUp=0'
 
