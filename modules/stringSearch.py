1b45662870ed8771e3a1b73e77bd5f2527e5f4aa
JustNao <nao.3nicolas@hotmail.fr>
2022-08-22 23:12:23 +0200

    Added sound effect for chat search

--- modules/stringSearch.py
+++ modules/stringSearch.py
@@ -1,20 +1,25 @@
-print('Importing sources ...')
+from playsound import playsound
 from sniffer import protocol
 from colorama import Fore
+import os
 
-print('Sources imported !')
 
 class Searcher:
-    def __init__(self, needle = '') -> None:
+    def __init__(self, needle='') -> None:
         self.update(needle)
 
     def packetRead(self, msg):
         name = protocol.msg_from_id[msg.id]["name"]
         if name == "ChatServerMessage":
             packet = protocol.read(name, msg.data)
-            found = any(str.lower() in packet['content'].lower() for str in self.needle)
+            found = any(
+                str.lower() in packet['content'].lower() for str in self.needle)
             if found:
-                print(Fore.LIGHTBLUE_EX + packet['senderName'] + Fore.RESET, ':', packet['content'])
+                path = os.path.abspath(
+                    os.path.join(os.getcwd(), 'sources/sound/msg.mp3'))
+                playsound(path)
+                print(Fore.LIGHTBLUE_EX +
+                      packet['senderName'] + Fore.RESET, ':', packet['content'])
 
     def update(self, string):
         needle = string.strip('\n')
